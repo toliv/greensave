fdb44d0043f6343fc37008462954f2e8680c572a
Tony Oliverio <tony.oliverio1@gmail.com>
2024-02-03 14:23:01 -0500

    .

--- web/green_save/tailwind.config.ts
+++ web/green_save/tailwind.config.ts
@@ -7,6 +7,21 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      "green": {
+        50: "#EEFFD6",
+        100: "#DEFFB3",
+        200: "#BAFD68",
+        300: "#93F325",
+        400: "#69BA12",
+        500: "#406D12",
+        600: "#3A660A",
+        700: "#366105",
+        800: "#305601",
+        900: "#2B4D00",
+        950: "#274200"
+      }
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
