59b870616910705ba9b0e86d30730d21b900e243
Tony Oliverio <tony.oliverio1@gmail.com>
2024-02-16 08:07:54 -0500

    fix useeffect for dynamic questions

--- web/green_save/src/app/appliance-finder/page.tsx
+++ web/green_save/src/app/appliance-finder/page.tsx
@@ -7,11 +7,8 @@ import {
 } from "@/schema/questionsSchema";
 import ZipCodeQuestion from "@/components/ZipCodeQuestion";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, Input } from "@material-tailwind/react";
-import { setDefaultAutoSelectFamilyAttemptTimeout } from "net";
 import { MutableRefObject, useEffect, useRef, useState } from "react";
-import { Controller, FormProvider, useForm } from "react-hook-form";
-import { z } from "zod";
+import { FormProvider, useForm } from "react-hook-form";
 
 export default function ApplianceFinder() {
   const methods = useForm<ApplianceFinder>({
@@ -30,7 +27,6 @@ export default function ApplianceFinder() {
   };
 
   const supportedEnergyTypes = methods.watch("supportedEnergyTypes");
-  // console.log(supportedEnergyTypes);
 
   // Use state to track the current question we're on
   const [questionIdx, setQuestionIdx] = useState<number>(0);
@@ -47,9 +43,9 @@ export default function ApplianceFinder() {
     MutableRefObject<HTMLDivElement | null>[]
   >([question1Ref, question2Ref, question3Ref]);
 
-  // by default
+  console.log(questionRefs.length);
+
   useEffect(() => {
-    console.log("CHANGING stuff");
     // If the user selects a particular energy type, we may need to ask extra questions
     // Start by resetting the questions in case things have been unchecked
     const defaultQuestions = [question1Ref, question2Ref, question3Ref];
@@ -67,11 +63,36 @@ export default function ApplianceFinder() {
       setQuestionRefs([...defaultQuestions, naturalGasHeaterQuestionRef]);
     } else if (userHasElectricity) {
       setQuestionRefs([...defaultQuestions, electricitySetupQuestionRef]);
+    } else {
+      setQuestionRefs(defaultQuestions);
     }
-    setQuestionRefs(defaultQuestions);
   }, [supportedEnergyTypes]);
 
+  const dynamicAdjustQuestions = () => {
+    // If the user selects a particular energy type, we may need to ask extra questions
+    // Start by resetting the questions in case things have been unchecked
+    const defaultQuestions = [question1Ref, question2Ref, question3Ref];
+    const userHasGas =
+      supportedEnergyTypes.includes("Natural Gas") ||
+      supportedEnergyTypes.includes("Propane");
+    const userHasElectricity = supportedEnergyTypes.includes("Electricity");
+    if (userHasGas && userHasElectricity) {
+      setQuestionRefs([
+        ...defaultQuestions,
+        naturalGasHeaterQuestionRef,
+        electricitySetupQuestionRef,
+      ]);
+    } else if (userHasGas) {
+      setQuestionRefs([...defaultQuestions, naturalGasHeaterQuestionRef]);
+    } else if (userHasElectricity) {
+      setQuestionRefs([...defaultQuestions, electricitySetupQuestionRef]);
+    } else {
+      setQuestionRefs(defaultQuestions);
+    }
+  };
+
   const moveToNextQuestion = () => {
+    // Use this as a trigger to reconsider questions
     if (questionIdx + 1 < questionRefs.length) {
       const nextQuestion = questionRefs[questionIdx + 1];
       nextQuestion.current?.scrollIntoView({
