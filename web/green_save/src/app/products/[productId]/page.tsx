a2720fe2a6e9042c4211d26ccc583d2d8733592f
Tony Oliverio <tony.oliverio1@gmail.com>
2024-03-08 21:47:53 -0500

    products page

--- web/green_save/src/app/products/[productId]/page.tsx
+++ web/green_save/src/app/products/[productId]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { WaterHeater } from "@/app/api/utils/productData";
 import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Checkbox } from "@material-tailwind/react";
+import { useState } from "react";
 
 async function getProductById(id: string): Promise<WaterHeater | undefined> {
   const response = await fetch(`/api/products/${id}`);
@@ -14,15 +16,69 @@ export default function Page({ params }: { params: { productId: string } }) {
     queryFn: () => getProductById(params.productId),
   });
 
+  const [sendEmail, setSendEmail] = useState<boolean>(false);
+  const [wantInstallation, setWantInstallation] = useState<boolean>(false);
+
   return (
     waterHeater && (
-      <div className="mt-40 text-white flex justify-around ">
-        <div>
-          <div>Product ID: {waterHeater.id}</div>
-          <div>Model Name: {waterHeater.modelName}</div>
-          <div>Power Source: {waterHeater.heaterType}</div>
+      <main className="min-h-screen bg-white text-black mt-20 p-8 lg:p-16">
+        <div className="flex flex-col gap-8 items-start">
+          <div className="flex flex-col gap-2 items-start">
+            <div className="text-2xl">
+              {waterHeater.brandName} {waterHeater.heaterType} Heater
+            </div>
+            <div className="text-sm text-default-gray">{`Model No. ${waterHeater.modelName}`}</div>
+          </div>
+          <div className="text-2xl">
+            <div>{`$1399`}</div>
+            <div className="text-lg text-default-gray">
+              <s>{`$1699`}</s>
+            </div>
+          </div>
+          <div className="text-lg">
+            Quick facts
+            <ul className="list-disc ml-6 mt-2">
+              <li>{`Fueled by: ${waterHeater.fuelTypes.toString()}`}</li>
+              {waterHeater.maximumGallonsPerMinute && (
+                <li>{`${waterHeater.maximumGallonsPerMinute} gallons per minute`}</li>
+              )}
+            </ul>
+          </div>
+
+          <div className="text-black text-md">
+            Why you'll love it:
+            <ul className="list-disc ml-6 mt-2">
+              <li>Comfortably serves your 2-shower household</li>
+              <li>Comfortably serves your 2-shower household</li>
+            </ul>
+          </div>
+          <div className="text-black text-lg">
+            <div className="mb-2">
+              Want to save this recommendation for later?
+            </div>
+            <div className="text-sm flex gap-4 items-center p-2">
+              <Checkbox
+                crossOrigin=""
+                checked={sendEmail}
+                onChange={() => setSendEmail(!sendEmail)}
+                className="text-sm"
+              />
+              <div>Send me an email with this recommendation</div>
+            </div>
+            <div className="text-sm flex gap-4 items-center p-2">
+              <Checkbox
+                crossOrigin=""
+                checked={wantInstallation}
+                onChange={() => setWantInstallation(!wantInstallation)}
+              />
+              <div>
+                I want Green$ave to contact me to help with purchase and
+                installation
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </main>
     )
   );
 }
