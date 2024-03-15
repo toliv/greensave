ac20cd2deccaf51140bf10634e03cedc9b20be25
Tony Oliverio <tony.oliverio1@gmail.com>
2024-03-14 20:27:13 -0400

    last commit before refactor

--- web/green_save/src/app/api/trpc/[trpc]/route.ts
+++ web/green_save/src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,25 @@
 import { appRouter } from "@/server";
-import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
-import { NextApiRequest, NextApiResponse } from "next";
+import * as trpcNext from "@trpc/server/adapters/next";
 
-const handler = (req: any) =>
-  fetchRequestHandler({
-    endpoint: "/api/trpc",
-    req,
-    router: appRouter,
-    createContext: () => ({}),
-  });
-
-export { handler as GET, handler as POST, handler as OPTIONS };
+export default trpcNext.createNextApiHandler({
+  router: appRouter,
+  /**
+   * @link https://trpc.io/docs/v11/context
+   */
+  createContext: () => ({}),
+  /**
+   * @link https://trpc.io/docs/v11/error-handling
+   */
+  onError({ error }) {
+    if (error.code === "INTERNAL_SERVER_ERROR") {
+      // send to bug reporting
+      console.error("Something went wrong", error);
+    }
+  },
+  /**
+   * @link https://trpc.io/docs/v11/caching#api-response-caching
+   */
+  // responseMeta() {
+  //   // ...
+  // },
+});
